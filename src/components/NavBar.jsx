29eac726a58db4e51cc217c43d2c7c6780f9c37b
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-15 08:43:24 +0800

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css"
+
+function NavBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/project">Project</a></li>
+                </ul>
+            </nav>
+        <Outlet />
+        </div>
+    );
+}
+
+
+
+export default NavBar;
