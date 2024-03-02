bd7e27580f7afbbdcfc62c2980879796f05c02cd
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-03-02 17:09:10 +0800

    Logo adjustment.

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function AuthPage(){
-    return  <div class="section">
-                <h1 class="error">Uh oh!</h1>
-                <div class="page">"Oops! It seems like you don't have the necessary authority to edit this content.</div>
-                <a class="back-home" href="/">Home is where the heart is.</a>
+    return  <div className="section">
+                <h1 className="error">Uh oh!</h1>
+                <div className="page">"Oops! It seems like you don't have the necessary authority to edit this content.</div>
+                <a className="back-home" href="/">Home is where the heart is.</a>
             </div>
 }
 
