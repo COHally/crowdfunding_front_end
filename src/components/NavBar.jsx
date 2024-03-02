4f31594df38d1bd738270bf0ca312ab1a46ba6e2
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-03-02 18:01:49 +0800

    Logo add.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
     return (
         <div>
             <div className="overlay">
-                <img src="https://github.com/COHally/crowdfunding_front_end/blob/main/src/images/Header%20image%203.png?raw=true" id="headerimage" />
+                <img src="src\images\Header image 4.png" id="headerimage" />
                 <h3>Crowdfunding for connection.</h3>
             </div>
             <nav>
